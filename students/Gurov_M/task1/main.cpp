2a6eeab117e1ca83b1ecdf241476561f8f6d9c3e
gurov-m <maks.gurov99@yandex.ru>
2018-02-17 01:51:46 +0300

    start

--- students/Gurov_M/task1/main.cpp
+++ students/Gurov_M/task1/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-
+#include <conio.h>
 using namespace std;
+class Converter
+{
+private:
+	int a;
+	double c;
+	double b;
+	double m;
+public:
+	void GetA();
+	void check(int *_a)
+	{
+		cout << *_a;
+	}
+
+};
+void Converter::GetA()
+{
+	cin >> a;
+}
+
+
+
+
+
 
 int main()
 {
-  cout << "Hello World!!!!!" << endl;
-  return 0;
+	Converter Ñ1;
+
+	Ñ1.check(&a);
+	system("pause");
 }
