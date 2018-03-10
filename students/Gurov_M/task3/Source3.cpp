722c30172a11328e02c1c533d6f0ca78ba8cbe61
gurov-m <maks.gurov99@yandex.ru>
2018-03-11 01:39:40 +0300

    Внес поправки

--- students/Gurov_M/task3/Source3.cpp
+++ students/Gurov_M/task3/Source3.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-void ArgumentEntry(double *_x);
-void NumberOfElementsEntry(int *_n);
-void NumberOfElementEntry(int *_n);
 class TaylorSeries
 {
 	double x;
 	int    n;
+	int choice;
 public:
-	TaylorSeries(double _argument = 0, int _number_of_elements = 0)
+	TaylorSeries(double _argument = 0, int _number_of_elements = 0, int _choice = 0)
 	{
 		x = _argument;
 		n = _number_of_elements;
+		choice = _choice;
 	}
 	~TaylorSeries()
 	{
@@ -22,95 +21,63 @@ public:
 	{
 		x = obj.x;
 		n = obj.n;
+		choice = obj.choice;
 		return *this;
 	}
 	int GetNumberOfElements()
 	{
 		return n;
 	}
-	double GetArgument()
+	void SetArgument(double _argument)
 	{
-		return x;
+		x = _argument;
 	}
-	double CalculateSin()
+	void SetChoice(int _choice)
 	{
-		double y = x;
-		double sum1 = 0.0;
-		for (int i = 1; i <= n; i++)
-		{
-			sum1 += y;
-			y *= -1.0 * x * x / ((2 * i) * (2 * i + 1));
-		}
-		return sum1;
+		choice = _choice;
 	}
-	double CalculateCos()
+	void SetNumberOfElements(int _number)
 	{
-		double y = 1.0;
-		double sum2 = 0.0;
-		for (int i = 1; i <= n; i++)
-		{
-			sum2 += y;
-			y *= -1.0 * x * x / ((2 * i - 1) * (2 * i));
-		}
-		return sum2;
+		n = _number;
 	}
-	double CalculateExponent()
-	{
-		double y = 1.0;
-		double sum3 = 1.0;
-		for (int i = 1; i <= n; i++)
-		{
-			y *= (x / i);
-			sum3 += y;
-		}
-		return sum3;
-	}
-	void SetArgument(double *_argument)
-	{
-		x = *_argument;
-	}
-	void SetNumberOfElements(int *_number)
-	{
-		n = *_number;
-	}
-	friend void PrintExponent(const TaylorSeries &q1);
-	friend void PrintSin(const TaylorSeries &q2);
-	friend void PrintCos(const TaylorSeries &q3);
-	friend  double ReferenceValueOfCos(const TaylorSeries &s1);
-	friend	double ReferenceValueOfSin(const TaylorSeries &s2);
-	friend	double ReferenceValueOfExponent(const TaylorSeries &s3);
-	double CalculateCurrentElementOfExponent(int *k)
+	friend  double CalculateSeries(const TaylorSeries &s1);
+	friend  void   PrintSeries(const TaylorSeries &q1);
+	friend  double ReferenceValue(const TaylorSeries &s1);
+	double CalculateCurrentElementOfExponent(int k)
 	{
 		double u = 1.0;
 		double i = 0.0;
-		int num = *k;
+		int num = k;
 		if (num == 1)
 			return 1.0;
 		else
+		{
 			i = pow(x, num);
-		for (int j = 1; j <= num; j++)
-			u *= j;
-		return (i / u);
+			for (int j = 1; j <= num; j++)
+				u *= j;
+			return (i / u);
+		}
 	}
-	double CalculateCurrentElementOfSin(int *k)
+	double CalculateCurrentElementOfSin(int k)
 	{
 		double u = 1.0;
 		double i = 0.0;
-		int num = *k;
+		int num = k;
 		if (num == 1)
 			return x;
 		else
-			return
+		{
 			i = pow(-1, (2 * num - 1))*pow(x, (2 * num - 1));
-		for (int j = 1; j <= (2 * num - 1); j++)
-			u *= j;
-		return (i / u);
+			for (int j = 1; j <= (2 * num - 1); j++)
+				u *= j;
+			return (i / u);
+		}
 	}
-	double CalculateCurrentElementOfCos(int *k)
+	double CalculateCurrentElementOfCos(int k)
 	{
 		double u = 1.0;
 		double i = 0.0;
-		int num = *k;
+		int num = k;
 		if (num == 1)
 			return 1.0;
 		else
@@ -121,73 +88,91 @@ public:
 		return (i / u);
 	}
 };
-double ReferenceValueOfCos(const TaylorSeries &s1)
+double CalculateSeries(const TaylorSeries &s1)
 {
+	double g = s1.x;
+	if (s1.choice == 1)
+		g = 1.0;
+
+	double sum2 = 0.0;
+	for (int i = 1; i <= s1.n; i++)
+	{
+		sum2 += g;
+		g *= -1.0 * s1.x * s1.x / ((2 * i - 1) * (2 * i));
+	}
+	return sum2;
+	if (s1.choice == 2)
+		g = s1.x;
+	double sum1 = 0.0;
+	for (int i = 1; i <= s1.n; i++)
+	{
+		sum1 += g;
+		g *= -1.0 * s1.x * s1.x / ((2 * i) * (2 * i + 1));
+	}
+	return sum1;
+	if (s1.choice == 3)
+		g = 1.0;
+	double sum3 = 1.0;
+	for (int i = 1; i <= s1.n; i++)
+	{
+		g *= (s1.x / i);
+		sum3 += g;
+	}
+	return sum3;
+}
+double ReferenceValue(const TaylorSeries &s1)
+{
+	if (s1.choice == 1)
+		return cos(s1.x);
+	if (s1.choice == 2)
+		return sin(s1.x);
+	if (s1.choice == 3)
+		return exp(s1.x);
 	return cos(s1.x);
 }
-double ReferenceValueOfSin(const TaylorSeries &s2)
+void   PrintSeries(const TaylorSeries &q1)
 {
-	return sin(s2.x);
-}
-double ReferenceValueOfExponent(const TaylorSeries &s3)
-{
-	return exp(s3.x);
-}
-void PrintExponent(const TaylorSeries &q1)
-{
-	cout << "1";
-	for (int i = 1; i < q1.n; i++)
+	if (q1.choice == 1)
 	{
-		cout << "+" << "x^" << i << "/" << i << "!";
+		cout << "1";
+		for (int i = 2; i <= q1.n; i++)
+		{
+			if (fmod(i, 2) == 0)
+				cout << "-";
+			else
+				cout << "+";
+			cout << "x^" << (2 * i - 2) << "/" << (2 * i - 2) << "!";
+		}
+		cout << endl;
 	}
-	cout << endl;
-}
-void PrintSin(const TaylorSeries &q2)
-{
-	cout << "x";
-	for (int i = 2; i <= q2.n; i++)
+	if (q1.choice == 2)
 	{
-		if (fmod(i, 2) == 0)
-			cout << "-";
-		else
-			cout << "+";
-		cout << "x^" << (2 * i - 1) << "/" << (2 * i - 1) << "!";
+		cout << "x";
+		for (int i = 2; i <= q1.n; i++)
+		{
+			if (fmod(i, 2) == 0)
+				cout << "-";
+			else
+				cout << "+";
+			cout << "x^" << (2 * i - 1) << "/" << (2 * i - 1) << "!";
+		}
+		cout << endl;
 	}
-	cout << endl;
-}
-void PrintCos(const TaylorSeries &q3)
-{
-	cout << "1";
-	for (int i = 2; i <= q3.n; i++)
+	if (q1.choice == 3)
 	{
-		if (fmod(i, 2) == 0)
-			cout << "-";
-		else
-			cout << "+";
-		cout << "x^" << (2 * i - 2) << "/" << (2 * i - 2) << "!";
+		cout << "1";
+		for (int i = 1; i < q1.n; i++)
+		{
+			cout << "+" << "x^" << i << "/" << i << "!";
+		}
+		cout << endl;
 	}
-	cout << endl;
-}
-void NumberOfElementsEntry(int *_n)
-{
-	cout << "Enter number of elements: ";
-	cin >> *_n;
-}
-void ArgumentEntry(double *_x)
-{
-	cout << "Enter argument x: ";
-	cin >> *_x;
-}
-void NumberOfElementEntry(int *_n)
-{
-	cout << "Enter number of element: ";
-	cin >> *_n;
 }
 int main()
 {
 	int choice = 0;
-	double _x;
-	int _n;
+	double _x = 0;
+	int _n = 0;
 	int  k = 0;
 	int h = 1;
 	int j = 0;
@@ -213,12 +198,15 @@ int main()
 				<< "Enter 2 = sinx  \n"
 				<< "Enter 3 = exponent" << endl;
 			cin >> choice;
+			T.SetChoice(choice);
 			system("pause");
 			system("cls");
 			break;
 		}
 		case 1:
 		{
+			if (choice == 0)
+				cout << "You didnt choose any function" << endl;
 			if (choice == 1)
 				cout << "cosx" << endl;
 			if (choice == 2)
@@ -231,8 +219,9 @@ int main()
 		}
 		case 2:
 		{
-			NumberOfElementsEntry(&_n);
-			T.SetNumberOfElements(&_n);
+			cout << "\n" << "Enter number of elements: ";
+			cin >> _n;
+			T.SetNumberOfElements(_n);
 			system("pause");
 			system("cls");
 			break;
@@ -246,60 +235,53 @@ int main()
 		}
 		case 4:
 		{
-			if (choice == 1)
-				PrintCos(T);
-			if (choice == 2)
-				PrintSin(T);
-			if (choice == 3)
-				PrintExponent(T);
+			cout << "\n" << "Enter number of elements: ";
+			cin >> _n;
+			T.SetNumberOfElements(_n);
+			PrintSeries(T);
 			system("pause");
 			system("cls");
 			break;
 		}
 		case 5:
 		{
-			NumberOfElementsEntry(&_n);
-			T.SetNumberOfElements(&_n);
-			ArgumentEntry(&_x);
-			T.SetArgument(&_x);
-			if (choice == 1)
-				cout << "\n" << T.CalculateCos() << endl;
-			if (choice == 2)
-				cout << "\n" << T.CalculateSin() << endl;
-			if (choice == 3)
-				cout << "\n" << T.CalculateExponent() << endl;
+			cout << "\n" << "Enter number of elements: ";
+			cin >> _n;
+			T.SetNumberOfElements(_n);
+			cout << "\n" << "Enter argument x: ";
+			cin >> _x;
+			T.SetArgument(_x);
+			cout << "\n" << CalculateSeries(T) << endl;
 			system("pause");
 			system("cls");
 			break;
 		}
 		case 6:
 		{
-			ArgumentEntry(&_x);
-			T.SetArgument(&_x);
-
-			NumberOfElementEntry(&k);
+			cout << "\n" << "Enter argument x: ";
+			cin >> _x;
+			T.SetArgument(_x);
+			cout << "\n" << "Enter number of element: ";
+			cin >> k;
 			if (choice == 1)
-				cout << "\n" << T.CalculateCurrentElementOfCos(&k) << endl;
+				cout << "\n" << T.CalculateCurrentElementOfCos(k) << endl;
 			if (choice == 2)
-				cout << "\n" << T.CalculateCurrentElementOfSin(&k) << endl;
+				cout << "\n" << T.CalculateCurrentElementOfSin(k) << endl;
 			if (choice == 3)
-				cout << "\n" << T.CalculateCurrentElementOfExponent(&k) << endl;
+				cout << "\n" << T.CalculateCurrentElementOfExponent(k) << endl;
 			system("pause");
 			system("cls");
 			break;
 		}
 		case 7:
 		{
-			NumberOfElementsEntry(&_n);
-			T.SetNumberOfElements(&_n);
-			ArgumentEntry(&_x);
-			T.SetArgument(&_x);
-			if (choice == 1)
-				cout << "\n" << abs(T.CalculateCos() - ReferenceValueOfCos(T)) << endl;
-			if (choice == 2)
-				cout << "\n" << abs(T.CalculateSin() - ReferenceValueOfSin(T)) << endl;
-			if (choice == 3)
-				cout << "\n" << abs(T.CalculateExponent() - ReferenceValueOfExponent(T)) << endl;
+			cout << "\n" << "Enter number of elements: ";
+			cin >> _n;
+			T.SetNumberOfElements(_n);
+			cout << "\n" << "Enter argument x: ";
+			cin >> _x;
+			T.SetArgument(_x);
+			cout << "\n" << abs(CalculateSeries(T) - ReferenceValue(T)) << endl;
 			system("pause");
 			system("cls");
 			break;
